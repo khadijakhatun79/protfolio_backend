bf22a495b9bafefc4fa4319947f9ea0a467446d1
khadijakhatun79 <khadijakhatunnurse@gmail.com>
2026-05-14 16:22:23 +0600

    update

--- src/app/modules/portfolio/portfolio.controller.js
+++ src/app/modules/portfolio/portfolio.controller.js
@@ -1,44 +1,47 @@
 const httpStatus = require("http-status");
 const catchAsync = require("../../../utils/catchAsync");
 const portfolioService = require("./portfolio.service");
-const { httpResponse } = require("../../../utils/httpResponse");
 
 const submitForm = catchAsync(async (req, res) => {
+  console.log("BODY:", req.body);
+
   const submission = await portfolioService.submitForm(req.body);
 
-  res.status(httpStatus.CREATED).json(
-    httpResponse("success", submission, "Form submitted successfully.")
-  );
+  res.status(httpStatus.CREATED).json({
+    success: true,
+    message: "Form submitted successfully",
+    data: submission,
+  });
 });
 
 const getSubmissions = catchAsync(async (req, res) => {
-  res.status(httpStatus.OK).json(
-    httpResponse("success", [], "All submissions fetched successfully.")
-  );
+  const result = await portfolioService.querySubmissions(req.query);
+
+  res.status(httpStatus.OK).json({
+    success: true,
+    message: "All submissions fetched successfully",
+    data: result,
+  });
 });
 
 const getSubmission = catchAsync(async (req, res) => {
-  const { id } = req.params;
+  const result = await portfolioService.getSubmissionById(req.params.submissionId);
 
-  res.status(httpStatus.OK).json(
-    httpResponse(
-      "success",
-      { id },
-      "Single submission fetched successfully."
-    )
-  );
+  res.status(httpStatus.OK).json({
+    success: true,
+    message: "Single submission fetched successfully",
+    data: result,
+  });
 });
 
 const deleteSubmission = catchAsync(async (req, res) => {
-  const { id } = req.params;
+  const result = await portfolioService.deleteSubmissionById(req.params.submissionId);
 
-  res.status(httpStatus.OK).json(
-    httpResponse(
-      "success",
-      { id },
-      "Submission deleted successfully."
-    )
-  );
+  res.status(httpStatus.OK).json({
+    success: true,
+    message: "Submission deleted successfully",
+    data: result,
+  });
 });
 
 module.exports = {
